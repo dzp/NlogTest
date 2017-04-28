24f162d5d4ab93f7892664ea740615a1f8aa64ee
戴子鹏 <daizipeng@roadrover.cn>
2017-04-28 18:06:59 +0800

    测试nlog

--- TestNlog/Program.cs
+++ TestNlog/Program.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using NLog;
 
 namespace TestNlog
 {
     class Program
     {
+        static Logger logger = LogManager.GetCurrentClassLogger();
         static void Main(string[] args)
         {
+            logger.Info("hello!");
+            while (true)
+            {
+                logger.Info("hello!");
+                Thread.Sleep(100);
+            }
+
+            return;
         }
     }
 }
